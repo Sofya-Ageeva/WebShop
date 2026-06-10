5dcf2189f1a572cb02add72c878c44d004553934
Агеева Софья <son4a1996@yandex.ru>
2026-06-10 20:54:45 +0300

    Создание контроллера приложения

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    """Контроллер для главной страницы"""
+    return render(request, 'catalog/home_page.html')
+
+
+def contacts(request):
+    """Контроллер для страницы контактов"""
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+
+        # Вывод в консоль (для отладки)
+        print(f"\nПолучено сообщение от {name} ({email}): {message}\n")
+
+        return render(request, 'catalog/contacts.html', {
+            'success': True,
+            'name': name
+        })
+
+    return render(request, 'catalog/contacts.html')
