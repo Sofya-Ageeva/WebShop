592223c7ff5db4f588a45684b865f725e797f5ee
Агеева Софья <son4a1996@yandex.ru>
2026-06-22 18:02:18 +0300

    Перевод имеющихся контроллеров с FBV на CBV

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,8 +4,10 @@ from . import views
 app_name = 'catalog'
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('contacts/', views.contacts, name='contacts'),
-    path('product/<int:pk>/', views.product_detail, name='product_detail'),
-    path('add/', views.add_product, name='add_product'),
+    path('', views.HomeView(), name='home'),
+    path('contacts/', views.ContactsView(), name='contacts'),
+    path('product/<int:pk>/', views.ProductDetailView.as_view(), name='product_detail'),
+    path('add/', views.ProductCreateView.as_view(), name='add_product'),
+    path('update/<int:pk>/', views.ProductUpdateView.as_view(), name='update_product'),
+    path('delete/<int:pk>/', views.ProductDeleteView.as_view(), name='delete_product'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,42 +1,52 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView, TemplateView
+from django.urls import reverse_lazy
+from django.shortcuts import render
 from .models import Product
 from .forms import ProductForm
 
-def home(request):
-    """Контроллер для главной страницы"""
-    products = Product.objects.all()
-    return render(request, 'catalog/home_page.html', {'products': products})
+class HomeView(ListView):
+    """Главная страница со списком товаров"""
+    model = Product
+    template_name = 'catalog/home_page.html'
+    context_object_name = 'products'
 
 
-def contacts(request):
-    """Контроллер для страницы контактов"""
-    if request.method == 'POST':
+class ContactsView(TemplateView):
+    """Страница контактов"""
+    template_name = 'catalog/contacts.html'
+
+    def post(self, request, *args, **kwargs):
         name = request.POST.get('name')
         email = request.POST.get('email')
         message = request.POST.get('message')
-
-        # Вывод в консоль (для отладки)
         print(f"\nПолучено сообщение от {name} ({email}): {message}\n")
-
-        return render(request, 'catalog/contacts.html', {
-            'success': True,
-            'name': name
-        })
-
-    return render(request, 'catalog/contacts.html')
-
-def product_detail(request, pk):
-    """Контроллер для страницы товара"""
-    product = get_object_or_404(Product, pk=pk)
-    return render(request, 'catalog/product_detail.html', {'product': product})
+        return render(request, self.template_name, {'success': True, 'name': name})
 
 
-def add_product(request):
-    if request.method == 'POST':
-        form = ProductForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return redirect('catalog:home')
-    else:
-        form = ProductForm()
-    return render(request, 'catalog/add_product.html', {'form': form})
+class ProductDetailView(DetailView):
+    """Страница товара"""
+    model = Product
+    template_name = 'catalog/product_detail.html'
+    context_object_name = 'product'
+
+
+class ProductCreateView(CreateView):
+    """Добавление товара"""
+    model = Product
+    form_class = ProductForm
+    template_name = 'catalog/add_product.html'
+    success_url = reverse_lazy('catalog:home')
+
+
+class ProductUpdateView(UpdateView):
+    """Редактирование товара"""
+    model = Product
+    form_class = ProductForm
+    template_name = 'catalog/product_update.html'
+    success_url = reverse_lazy('catalog:home')
+
+class ProductDeleteView(DeleteView):
+    """Удаление товара"""
+    model = Product
+    template_name = 'catalog/product_delete.html'
+    success_url = reverse_lazy('catalog:home')
