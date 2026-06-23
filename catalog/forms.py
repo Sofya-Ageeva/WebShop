8513f4fd2e7296a96c6678e7849a9e278c48d685
Агеева Софья <son4a1996@yandex.ru>
2026-06-23 18:43:35 +0300

    Валидация проверки наличия запрещенных слов

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,5 +1,18 @@
 from django import forms
+from django.core.exceptions import ValidationError
 from .models import Product
+
+FORBIDDEN_WORDS = [
+    'казино',
+    'криптовалюта',
+    'крипта',
+    'биржа',
+    'дешево',
+    'бесплатно',
+    'обман',
+    'полиция',
+    'радар',
+]
 
 class ProductForm(forms.ModelForm):
     class Meta:
@@ -18,3 +31,50 @@ class ProductForm(forms.ModelForm):
             'category': 'Категория',
             'price': 'Цена',
         }
+
+    def __init__(self, *args, **kwargs):
+        """Добавляем стили Bootstrap ко всем полям"""
+        super().__init__(*args, **kwargs)
+
+        for field_name, field in self.fields.items():
+            if field.widget.__class__.__name__ != 'CheckboxInput':
+                if 'class' not in field.widget.attrs:
+                    field.widget.attrs['class'] = ''
+                if 'form-control' not in field.widget.attrs['class']:
+                    field.widget.attrs['class'] += ' form-control'
+
+    def clean_name(self):
+        """Валидация названия продукта на запрещённые слова"""
+        name = self.cleaned_data.get('name')
+        if name:
+            name_lower = name.lower()
+            for forbidden in FORBIDDEN_WORDS:
+                if forbidden in name_lower:
+                    raise ValidationError(
+                        f'Название содержит запрещённое слово: "{forbidden}". '
+                        f'Пожалуйста, удалите его.'
+                    )
+        return name
+
+    def clean_description(self):
+        """Валидация описания продукта на запрещённые слова"""
+        description = self.cleaned_data.get('description')
+        if description:
+            desc_lower = description.lower()
+            for forbidden in FORBIDDEN_WORDS:
+                if forbidden in desc_lower:
+                    raise ValidationError(
+                        f'Описание содержит запрещённое слово: "{forbidden}". '
+                        f'Пожалуйста, удалите его.'
+                    )
+        return description
+
+    def clean_price(self):
+        """Валидация цены"""
+        price = self.cleaned_data.get('price')
+        if price is not None and price < 0:
+            raise ValidationError(
+                'Цена не может быть отрицательной. '
+                'Пожалуйста, введите корректную цену.'
+            )
+        return price
